4ac91dc65ba9d6c9570afce9f7fa434f485fed13
paulleerentveld <paul@veldnet.com.au>
2022-06-12 10:42:44 +1000

    lab done

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,16 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+def speak_to_grandma(input) 
+    if input == "I LOVE YOU GRANDMA!"
+        return "I LOVE YOU TOO PUMPKIN!"
+    elsif input == input.upcase
+        return "NO, NOT SINCE 1938!"
+    else 
+        return "HUH?! SPEAK UP, SONNY!" 
+    end
+end
+
+
